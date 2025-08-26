aeb884b64c80994d41947b5c700b1a1483aaffb5
Matheus Leao <matsouzaleao@gmail.com>
2025-08-26 20:26:49 -0300

    Create get request using fixture

--- cypress/e2e/products.cy.ts
+++ cypress/e2e/products.cy.ts
@@ -13,6 +13,18 @@ describe('Product Tests', () => {
             expect(producResponseBody.products.length).to.eq(30)
           })
         })
+        it('When I make a get request to id=1, should return 1 unique product', ()=>{
+          cy.fixture('requests/get/whenIMakeGetRequestToId1.json').then((expectedGetResponse)=>{
+            cy.request({
+              url: `/products/${expectedGetResponse.id}`,
+              method: 'GET'
+            }).then((response) => {
+              expect(response.status).to.eq(200);  
+              expect(response.body).to.deep.equal(expectedGetResponse)
+            })
+          })
+
+        })
 
     })
 })
