bb8413c06efb390d0d4fc617ac4ce33a9aba19ab
Matheus Leao <matsouzaleao@gmail.com>
2025-08-26 19:46:24 -0300

    Create first product test

--- cypress/e2e/products.cy.ts
+++ cypress/e2e/products.cy.ts
@@ -1,5 +1,15 @@
-describe('', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+import { expect } from 'chai';
+
+describe('Product Tests', () => {
+    describe('GET /products', ()=>{
+        it('When I make a get request, should return all products', () => {
+          cy.request({
+            url: '/products',
+            method: 'GET'
+          }).then((response) => {
+            expect(response.status).to.eq(200);
+          })
+        })
+
+    })
 })
