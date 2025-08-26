2fc399c63d9c60f20d0c0ab752e5eaed30ee8177
Matheus Leao <matsouzaleao@gmail.com>
2025-08-26 20:15:26 -0300

    Update get all products test

--- cypress/e2e/products.cy.ts
+++ cypress/e2e/products.cy.ts
@@ -1,13 +1,16 @@
 import { expect } from 'chai';
+import { ProductResponseBody as ProductResponseBody } from '../support/model/productModel';
 
 describe('Product Tests', () => {
     describe('GET /products', ()=>{
-        it('When I make a get request, should return all products', () => {
+        it('When I make a get request, should return 30 products', () => {
           cy.request({
             url: '/products',
             method: 'GET'
           }).then((response) => {
             expect(response.status).to.eq(200);
+            const producResponseBody = response.body as ProductResponseBody;
+            expect(producResponseBody.products.length).to.eq(30)
           })
         })
 
