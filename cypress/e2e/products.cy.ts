2c97ddbafb5231d79441d919fe88a8afbb1ebf0d
Matheus Leao <matsouzaleao@gmail.com>
2025-08-26 21:16:46 -0300

    Refactor get products to be an cypress command

--- cypress/e2e/products.cy.ts
+++ cypress/e2e/products.cy.ts
@@ -3,10 +3,8 @@ import { ProductResponseBody as ProductResponseBody } from '../support/model/pro
 describe('Product Tests', () => {
     describe('Get products', ()=>{
         it('When I make a get request, should return 30 products', () => {
-          cy.request({
-            url: '/products',
-            method: 'GET'
-          }).then((response) => {
+          cy.getProducts()
+          .then((response) => {
             expect(response.status).to.eq(200);
             const productResponseBody = response.body as ProductResponseBody;
             expect(productResponseBody.products.length).to.eq(30)
@@ -25,13 +23,7 @@ describe('Product Tests', () => {
         })
         it('When I make a get request with 1 limit, should return 1 products', () => {
           const limitExpected = 1;
-          cy.request({
-            url: '/products',
-            method: 'GET',
-            qs: {
-              limit: limitExpected
-            }
-          }).then((response) => {
+          cy.getProducts({limit: limitExpected}).then((response) => {
             expect(response.status).to.eq(200);
             const productResponseBody = response.body as ProductResponseBody;
             expect(productResponseBody.products.length).to.eq(limitExpected)
@@ -40,26 +32,12 @@ describe('Product Tests', () => {
         it('When I make a get request with limit=1 and skip=1, should not the same product product (id=2)', () => {
           const limitExpected = 1;
           const skipExpected = 0
-          cy.request({
-            url: '/products',
-            method: 'GET',
-            qs: {
-              limit: limitExpected,
-              skip: skipExpected
-            }
-          }).then((response) => {
+          cy.getProducts({limit: limitExpected, skip: skipExpected}).then((response) => {
             expect(response.status).to.eq(200);
             const productResponseBody = response.body as ProductResponseBody;
             expect(productResponseBody.products.length).to.eq(limitExpected);
             const firstProductId = productResponseBody.products[0].id;
-            cy.request({
-              url: '/products',
-              method: 'GET',
-              qs: {
-                limit: limitExpected,
-                skip: skipExpected + 1
-              }
-            }).then((responseForSecondSkip) => {
+            cy.getProducts({limit: limitExpected, skip: skipExpected + 1}).then((responseForSecondSkip) => {
               const secondProduct = (responseForSecondSkip.body as ProductResponseBody).products[0];
               // The product after skipping one should not be the same as the first
               expect(firstProductId).to.not.eq(secondProduct.id)
