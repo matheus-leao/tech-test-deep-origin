58493a313b6a9d95a117de57b78b37d47f9d20d1
Matheus Leao <matsouzaleao@gmail.com>
2025-08-28 21:17:15 -0300

    Create productUpdate tests

--- cypress/e2e/product/productsUpdate.cy.ts
+++ cypress/e2e/product/productsUpdate.cy.ts
@@ -2,24 +2,76 @@ import { faker } from "@faker-js/faker";
 import { ProductDto } from "../../support/model/productModel";
 
 describe("Product Update", () => {
-  it("When I update the title of a product, should return 201", () => {
-    const productModelWithEssentialFields: ProductDto = {
-      title: "Test product Creation",
-      description: faker.lorem.paragraph(),
-      category: "fakeCategory",
-      price: 8.99,
-      brand: "Fake Brand",
-    };
-    cy.request({
-      url: "/products/add",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: productModelWithEssentialFields,
-    }).then((response) => {
-      expect(response.status).to.eq(201);
-      expect(response.body).to.deep.include(productModelWithEssentialFields);
-    });
+  it("When I update the title of a product, should return 200 - PUT", () => {
+    const newTitle = faker.lorem.words(2)
+    const productId = 2;
+      cy.request({
+        url: `/products/${productId}`,
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: {title: newTitle},
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        const updatedProduct: ProductDto = response.body;
+        expect(updatedProduct.title).to.be.equal(newTitle);
+      });
   });
+
+  it("When I update the title of a product, should return 200 - PATCH", () => {
+    const newTitle = faker.lorem.words(2)
+    const productId = 2;
+      cy.request({
+        url: `/products/${productId}`,
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: {title: newTitle},
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        const updatedProduct: ProductDto = response.body;
+        expect(updatedProduct.title).to.be.equal(newTitle);
+      });
+  });
+
+  it("When I update the title and description of a product, should return 200 - PUT", () => {
+    const newTitle = faker.lorem.words(2)
+    const newDescription = faker.lorem.paragraph(2)
+    const productId = 2;
+      cy.request({
+        url: `/products/${productId}`,
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: {title: newTitle, description: newDescription},
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        const updatedProduct: ProductDto = response.body;
+        expect(updatedProduct.title).to.be.equal(newTitle);
+        expect(updatedProduct.description).to.be.equal(newDescription);
+      });
+  });
+
+   it("When I update the title and description of a product, should return 200 - PUT", () => {
+    const newTitle = faker.lorem.words(2)
+    const newDescription = faker.lorem.paragraph(2)
+    const productId = 2;
+      cy.request({
+        url: `/products/${productId}`,
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: {title: newTitle, description: newDescription},
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        const updatedProduct: ProductDto = response.body;
+        expect(updatedProduct.title).to.be.equal(newTitle);
+        expect(updatedProduct.description).to.be.equal(newDescription);
+      });
+  });
+
 });
