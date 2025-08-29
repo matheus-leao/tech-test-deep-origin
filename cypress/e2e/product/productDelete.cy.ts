046459df6dd46c005bd9db116505b7f66ea15bbb
Matheus Leao <matsouzaleao@gmail.com>
2025-08-28 21:50:44 -0300

    Create product delete tests

--- cypress/e2e/product/productDelete.cy.ts
+++ cypress/e2e/product/productDelete.cy.ts
@@ -1,5 +1,7 @@
+import { ProductResponseBodyDto } from "../../support/model/productModel";
+
 describe("Delete products", () => {
-    it("When I make a get request, should return 30 products", () => {
+    it("When I try to delete an existent product, should return 200", () => {
         const productId = 2;
         cy.request({
         url: `/products/${productId}`,
@@ -14,5 +16,23 @@ describe("Delete products", () => {
         const deletedOnTime = new Date(response.body.deletedOn).getTime();
         expect(deletedOnTime).to.be.closeTo(new Date().getTime(), 1000);
       });
+    });
+
+    it("When I try to deleta a non-existent product, should return 404", () => {
+        cy.getProducts().then((getProductsResponse)=>{
+          const productResponseDto: ProductResponseBodyDto  = getProductsResponse.body;
+          const nonExistentProductId = productResponseDto.total + 1;
+          cy.request({
+          url: `/products/${nonExistentProductId}`,
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          failOnStatusCode: false
+        }).then((response) => {
+          expect(response.status).to.eq(404);
+          expect(response.body).to.deep.include({message: `Product with id '${nonExistentProductId}' not found`})
+        });
+        })
     });
 });
