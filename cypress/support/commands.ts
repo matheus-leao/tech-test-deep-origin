f84e70ebe30a2d1700aede7cd9b6475f27408725
Matheus Leao <matsouzaleao@gmail.com>
2025-08-28 21:15:38 -0300

    Create getProduct command

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,12 +1,22 @@
 declare namespace Cypress {
   interface Chainable {
     getProducts(options?: object): Chainable<Response<any>>;
+    getProduct(productId:number, options?: object): Chainable<Response<any>>;
   }
 }
 
 Cypress.Commands.add("getProducts", (options = {}) => {
   return cy.request({
     url: `/products/`,
+    method: "GET",
+    qs: options,
+  });
+});
+
+//alterar outros get product pra usar essa funcao #todo
+Cypress.Commands.add("getProduct", (productId, options = {}) => {
+  return cy.request({
+    url: `/products/${productId}/`,
     method: "GET",
     qs: options,
   });
